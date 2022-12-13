17e1cc5b6cfff5992ef4420b29268c2f675dec33
Nicegomting <ggoo.kang@gmail.com>
2022-12-13 10:23:56 +0900

    Information Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("WinForm2022")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FractionalInches")]
+[assembly: AssemblyDescription("Fractional Inches Converter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinForm2022")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("Fractional Inches Converter")]
+[assembly: AssemblyCopyright("Copyright (C)  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
